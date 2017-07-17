38f6faf5453a3ad8ed423833b0bf5207a97fc8f8
kawao <tomoyuki.kawao@gmail.com>
2017-07-17 21:00:13 +0900

    fix bug.

--- src/key-code-array.h
+++ src/key-code-array.h
@@ -30,7 +30,7 @@ typedef GArray KeyCodeArray;
 #define key_code_array_free(array) g_array_free((array), TRUE)
 #define key_code_array_append(array, key_code)          \
   (key_code_array_contains((array), (key_code))         \
-   ? g_array_append_val((array), (key_code)) : FALSE)
+   ? FALSE : g_array_append_val((array), (key_code)))
 #define key_code_array_get_data(array, index)   \
   g_array_index((array), KeyCode, (index))
 #define key_code_array_get_length(array) ((array)->len)
