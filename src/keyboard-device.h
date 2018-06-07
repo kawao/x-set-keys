5e3c58cf36fd38ff9c6718b004190f33f52e1574
kawao <tomoyuki.kawao@gmail.com>
2018-06-07 20:27:34 +0900

    Updated copyright year

--- src/keyboard-device.h
+++ src/keyboard-device.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * Copyright (C) 2017 Tomoyuki KAWAO
+ * Copyright (C) 2017-2018 Tomoyuki KAWAO
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
